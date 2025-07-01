f1029d0c4a5690a4107fd91613aacff3ce0dfa97
azard285 <pavlenkopasa382@gmail.com>
2025-07-01 17:05:10 +0700

    погнал elexir, сейчас будем разбираться. пока что осваиваем базу, вывод сума и тд

---
+++ 1lvl/sum.ex
@@ -0,0 +1,7 @@
+defmodule Sum do
+  def dva(a, b) do
+    IO.puts(a+b)
+  end
+end
+
+Sum.dva(1,2)
---
+++ 1lvl/vivod.ex
@@ -0,0 +1,7 @@
+defmodule Hi do
+  def vivod() do
+    IO.puts("darova samarityni")
+  end
+end
+
+Hi.vivod()
